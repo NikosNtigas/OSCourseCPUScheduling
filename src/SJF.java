e4e5186d51e6386fe06135c85973835fd9c2b2c6
igpapadi <43881975+igpapadi@users.noreply.github.com>
2020-12-20 14:28:04 +0200

    Update SJF.java

--- src/SJF.java
+++ src/SJF.java
@@ -1,17 +1,34 @@
 
 public class SJF extends Scheduler {
-
     public SJF() {
         /* TODO: you _may_ need to add some code here */
     }
 
     public void addProcess(Process p) {
+        processes.add(p);
         /* TODO: you need to add some code here */
     }
     
     public Process getNextProcess() {
-        /* TODO: you need to add some code here
-         * and change the return value */
-        return null;
+        int min_burst_time = Integer.MAX_VALUE;
+        int position=0;
+       // boolean flag=false;
+if (processes.isEmpty())
+    return  null;
+
+             /* TODO: you need to add some code here
+             *and change the return value */
+
+    for (int i = 0; i < processes.size(); i++) {
+            if (processes.get(i).getBurstTime() < min_burst_time) {
+                min_burst_time = processes.get(i).getBurstTime();
+                position = i;
+            }
+
     }
+        System.out.print(position + ": position\n");
+            return processes.get(position);
+
+        }
+
 }
